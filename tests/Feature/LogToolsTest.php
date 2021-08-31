0a5eec21c30e79217e20b2ec06f74a12d405db06
ECPAY\shawn.chang <shawn.chang@ecpay.com.tw>
2021-08-31 10:57:58 +0800

    fix: Fix LogTool test case
    reason: Use wrong field name
    
    changed:
    1.Fix field name
    2.Update laravel/framework from 6.20.15 to 6.20.32

--- tests/Feature/LogToolsTest.php
+++ tests/Feature/LogToolsTest.php
@@ -11,16 +11,16 @@ class LogToolsTest extends TestCase
         // input
         $url = route('v1.legacy-query-sync-generator');
         $queryData = [
-            'type' => 'test',
-            'start' => '2021/01/20 00:00:00',
-            'end' => '2021/01/21 00:00:00',
+            'logType' => 'test',
+            'startDate' => '2021/01/20 00:00:00',
+            'endDate' => '2021/01/21 00:00:00',
             'logData' => '20210120091746',
         ];
         $queryString = http_build_query($queryData);
 
         // expected
         $findSource = [
-            'type' => '"type": "' . $queryData['type'] . '"',
+            'type' => '"type": "' . $queryData['logType'] . '"',
             'date' => '"created_at": {$gt: 1611072000.0, $lt: 1611158400.0}',
             'logData' => '"log_data": /' . $queryData['logData'] . '/',
         ];
